300f42b8adf03ba77b78d9fdd462eabf02828442
João Leite <joaoaugustobr@hotmail.com>
2022-05-25 20:52:56 -0300

    added logger

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -1,6 +1,32 @@
 import random
+from typing import Optional
 import numpy as np
 import torch
+import logging
+
+
+def get_logger(level: Optional[str] = "debug", filename: Optional[str] = None) -> logging.Logger:
+    level_map = {
+        "debug": logging.DEBUG,
+        "info": logging.INFO,
+        "warning": logging.WARNING,
+        "error": logging.ERROR,
+        "fatal": logging.FATAL,
+    }
+    loglevel = level_map.get(level)
+    if loglevel is None:
+        raise TypeError
+
+    logging.basicConfig(
+        level=loglevel,
+        filename=filename,
+        filemode="w" if filename else None,
+        format="%(levelname)s | %(asctime)s - %(message)s",
+        datefmt="%d-%b-%y %H:%M:%S",
+    )
+
+    logger = logging.getLogger()
+    return logger
 
 
 def set_seed(seed_value):
